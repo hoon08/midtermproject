953279add0acf0930f442d1f2ed183168dd690f8
hoon08 <hoon_09@naver.com>
2015-05-06 11:20:51 -0400

    5.7 am12

---
+++ img_proc.c
@@ -0,0 +1,10 @@
+/*
+ * img_proc.c
+ *
+ *  Created on: May 5, 2015
+ *      Author: user
+ */
+
+
+    void rgb2ycbcr(char *image, int cols, int rows);
+    void ycbcr2rgb(char *input, int cols, int rows);
---
+++ img_proc.h
@@ -0,0 +1,8 @@
+/*
+ * img_proc.h
+ *
+ *  Created on: May 5, 2015
+ *      Author: user
+ */
+
+
